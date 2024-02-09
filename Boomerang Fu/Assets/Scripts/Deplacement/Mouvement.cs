66d370cf2dcc2b145f2e5ef7b1001b2e2bb7a0a1
Your Name <you@example.com>
2024-02-09 15:45:35 +0100

    Fin du Dash et du déplacement

--- Boomerang Fu/Assets/Scripts/Deplacement/Mouvement.cs
+++ Boomerang Fu/Assets/Scripts/Deplacement/Mouvement.cs
@@ -6,16 +6,26 @@ using UnityEngine.InputSystem;
 public class PlayerMouvement : MonoBehaviour
 {
     public Rigidbody Rb;
+    private PlayerDash _dash;
     public float Vitesse = 5;
     private void Awake()
     {
         var _input = GetComponentInChildren<PlayerInputHandler>();
         _input.OnMove += Movement;
+        _dash = GetComponent<PlayerDash>();
     }
     void Movement(InputAction.CallbackContext Moving)
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-        Rb.MovePosition(new Vector3(x,transform.position.y,z) * Vitesse * Time.deltaTime);
+        if(_dash._timer <= _dash.Cooldown - (_dash.Cooldown / 4)) { 
+            var dir = Moving.ReadValue<Vector2>();
+            Rb.velocity = (new Vector3(dir.x, 0, dir.y) * Vitesse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Rb.velocity.magnitude > 0.1) { 
+            this.transform.rotation = Quaternion.LookRotation(Rb.velocity, Vector3.up);
+        }
     }
 }
