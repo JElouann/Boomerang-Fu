88b8eda2f5d800e36fceade8fbd6f09627b6a3a3
elagmae <lea.pouthier@gmail.com>
2024-02-15 12:05:25 +0100

    Changements UI + fix responsive

--- Boomerang Fu/Assets/Scripts/PauseScreen.cs
+++ Boomerang Fu/Assets/Scripts/PauseScreen.cs
@@ -3,34 +3,46 @@ using UnityEngine.InputSystem;
 
 public class PauseScreen : MonoBehaviour
 {
-    [SerializeField] private GameObject _uiPause; // on r�cup�re le panel d'UI pour la pause
+    // on récupère le panel d'UI pour la pause
+    private GameObject _uiPause;
 
-    public void Pause(InputAction.CallbackContext value) //Pour ajouter et retirer la pause gr�ce � l'input associ�
+    private void Awake()
     {
-        if(_uiPause.activeSelf == true) // si l'�cran est d�j� en pause
+        // on cherche le panel d'UI pour la pause
+        _uiPause = GameObject.FindGameObjectWithTag("PauseScreen");
+    }
+
+    //Pour ajouter et retirer la pause grâce à l'input associé
+    public void Pause(InputAction.CallbackContext value) 
+    {
+        // si l'écran est déjà en pause
+        if(_uiPause.transform.GetChild(0).gameObject.activeSelf == true) 
         {
-            UnPause(); // on appelle l'autre méthode pour éviter les doublons
+            // on appelle l'autre méthode pour éviter les doublons
+            UnPause(); 
         }
 
-        else // sinon
+        else
         {
-            // on met l'�cran en pause
+            // on met l'écran en pause
             Time.timeScale = 0.0f;
-            _uiPause.SetActive(true);
+            _uiPause.transform.GetChild(0).gameObject.SetActive(true);
         }
     }
 
     public void UnPause() 
     {
         // on peut rejouer
-        _uiPause.SetActive(false);
+        _uiPause.transform.GetChild(0).gameObject.SetActive(false);
         Time.timeScale = 1.0f;
     }
 
     private void Start()
     {
-        var _input = GetComponentInChildren<PlayerInputHandler>(); // on r�cup�re l'input handler
-        _input.OnPause += Pause; // ajouter la m�thode de pause � l'input de pause
+        // on récupère l'input handler
+        var _input = GetComponentInChildren<PlayerInputHandler>();
+        // ajouter la méthode de pause à l'input de pause
+        _input.OnPause += Pause;
     }
 
 }
