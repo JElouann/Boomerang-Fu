e3ec547c15341128ed35fa7b09cd0ed845817701
Atylla <cutie.lyta@gmail.com>
2024-02-15 11:38:35 +0100

    Fix finUI

--- Boomerang Fu/Assets/Scripts/FinUI.cs
+++ Boomerang Fu/Assets/Scripts/FinUI.cs
@@ -3,22 +3,22 @@ using UnityEngine;
 
 public class FinUI : MonoBehaviour
 {
-    private void ShowTrophy(int winner)
+    public void ShowTrophy(int winner)
     {
         // on récupère le parent qui contient les trophées
-       var trophees = GameObject.Find("Trophées");
+        var trophees = GameObject.Find("Trophées");
 
         // on affiche le trophée du gagnant
         trophees.transform.GetChild(winner).gameObject.SetActive(true);
     }
 
-    private void ScoreFinal()
+    public void ScoreFinal()
     {
         // on récupère le parent qui contient les scores
         var scores = GameObject.Find("Scores");
 
         // on associe chaque texte de score à un score
-        for (int i = 0; i> GameManager.Instance.Score.Count; i++)
+        for (int i = 0; i > GameManager.Instance.Score.Count; i++)
         {
             scores.transform.GetChild(i).GetComponent<TextMeshProUGUI>().text = GameManager.Instance.Score[i].ToString();
         };
